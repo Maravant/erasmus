78313418bd5c1ed1209e48e30c4e334acc306bad
Eduardo Sada <eduardosada@gmail.com>
2017-04-01 15:03:18 +0200

    feat(menu): agregado menu para mobile

--- js/header.js
+++ js/header.js
@@ -18,4 +18,28 @@
     }
   });
 
+  window.MAIN_APP.push(function initMenuForMobile() {
+    var header = document.querySelector('.header');
+    var menuButton = document.querySelector('.header__menubutton');
+    var menu = document.querySelector('.header__menu');
+    var menuList = document.querySelector('.header__menulist');
+
+    menuButton.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      if (header.classList.contains('header--opened')) {
+        menu.prepend(menuList);
+        header.classList.remove('header--opened');
+        document.body.classList.remove('menu--opened')
+      } else {
+        header.prepend(menuList);
+        header.classList.add('header--opened');
+        document.body.classList.add('menu--opened')
+        window.scrollTo(0, 0);
+      }
+
+    })
+
+  });
+
 }());
