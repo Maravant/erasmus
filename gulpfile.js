cdf1464f4730b2679d6c3ff1637cab84013689d2
Eduardo Sada <eduardosada@gmail.com>
2017-04-02 17:08:02 +0200

    feat(all): mas puntos en lighthouse

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,23 @@ gulp.task('css', function () {
         .pipe( sourcemaps.write('.') )
         .pipe( gulp.dest('build/') );
 });
+
+gulp.task('generate-service-worker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+
+  swPrecache.write('sw.js', {
+    staticFileGlobs: [
+      'https://code.jquery.com/jquery-3.1.1.slim.min.js',
+      'build/*.{css,map}',
+      'components/*.{css,map}',
+      'elements/*.{css,map}',
+      'generic/*.{css,map}',
+      'images/*.{png,jpg,gif,svg}',
+      'pages/*.{css,map}',
+      'settings/*.{css,map}',
+      'js/*.{js,html,css,png,jpg,gif,svg}',
+      '*.{js,html,css,ico}'
+    ]
+  }, callback);
+});
