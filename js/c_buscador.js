e8d7b038ab44c9c2f5b531b1374b7f46d1cde802
Eduardo Sada <eduardosada@gmail.com>
2017-04-20 15:15:18 +0200

    feat(buscador): buscador auxiliares para iphone5 + js de emails

--- js/c_buscador.js
+++ js/c_buscador.js
@@ -18,6 +18,44 @@
       $('.c_buscador').toggleClass('c_buscador--visible');
       e.preventDefault();
     });
+
+
+    var users = [];
+    var updateCCIcon = function() {
+      if (users.length) {
+        $('.c_results-messages').addClass('button--visible');
+      } else {
+        $('.c_results-messages').removeClass('button--visible');
+      }
+
+      $('.c_results-messages__circle').text(users.length);
+    };
+
+
+    $('.js-add-to-email-cc').on('click', function(e) {
+      e.preventDefault();
+
+      var button = $(e.currentTarget);
+      var cur = button.closest('.c_item');
+      var id = cur.data('item-id');
+
+      if (typeof id === 'undefined') {
+        return ;
+      }
+
+      var index = users.indexOf(id);
+
+      if (index > -1) {
+        users.splice(index, 1);
+        button.removeClass('button--active');
+      } else {
+        users.push(id);
+        button.addClass('button--active');
+      }
+
+      updateCCIcon();
+    });
+
   });
 
 }());
