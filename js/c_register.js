5643df4e28cc5a43b34b9a4c3428e77ccf1b4946
Eduardo Sada <eduardosada@gmail.com>
2017-05-15 10:09:48 +0200

    fix(register): move js to file

--- js/c_register.js
+++ js/c_register.js
@@ -1,7 +1,26 @@
-function registerOptionSelected() {
-  $('.c_register__option').removeClass('c_register__option--selected');
-  $(this).addClass('c_register__option--selected');
-}
+(function() {
+  window.MAIN_APP = window.MAIN_APP || [];
+
+  window.MAIN_APP.push(function initLogin() {
+    function registerOptionSelected() {
+      $('.c_register__option').removeClass('c_register__option--selected');
+      $(this).addClass('c_register__option--selected');
+    }
+    $('.c_register__option').on('click', registerOptionSelected);
 
 
-$('.c_register__option').on('click', registerOptionSelected);
+
+    $('.js-click-condition').on('click', function(e) {
+      if ($('.js-click-condition').prop('checked')){
+        $('.button-submit').removeAttr('disabled');
+        $('.c_register-form__validation-box__alert').addClass('correct');
+        $('.c_register-form__validation-box__alert').removeClass('error');
+      } else {
+        $('.button-submit').attr('disabled', 'disabled');
+        $('.c_register-form__validation-box__alert').removeClass('correct');
+        $('.c_register-form__validation-box__alert').addClass('error');
+      }
+    });
+  });
+
+}());
