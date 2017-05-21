ef579c6023c6c539357688ac4ff7edf64cfb5d86
Eduardo Sada <eduardosada@gmail.com>
2017-05-21 23:13:06 +0200

    feat(registro): agrega validacion en formulario para campos requeridos

--- js/c_register.js
+++ js/c_register.js
@@ -2,25 +2,57 @@
   window.MAIN_APP = window.MAIN_APP || [];
 
   window.MAIN_APP.push(function initLogin() {
+    function showError(type) {
+      var types = ['correct', 'error', 'required'];
+      var box = $('.c_register-form__validation-box__alert');
+
+      $(types).each(function(i, value) {
+        if (type === value) {
+          box.addClass(value);
+        } else {
+          box.removeClass(value);
+        }
+      });
+    };
+
     function registerOptionSelected() {
       $('.c_register__option').removeClass('c_register__option--selected');
       $(this).addClass('c_register__option--selected');
-    }
+    };
+
     $('.c_register__option').on('click', registerOptionSelected);
 
-
-
     $('.js-click-condition').on('click', function(e) {
-      if ($('.js-click-condition').prop('checked')){
-        $('.button-submit').removeAttr('disabled');
-        $('.c_register-form__validation-box__alert').addClass('correct');
-        $('.c_register-form__validation-box__alert').removeClass('error');
+      if ($('.js-click-condition').prop('checked')) {
+
+        var form = $(this).closest('form');
+        var formValid = form[0].checkValidity();
+
+        // if (formValid) {
+          $('.button-submit').removeAttr('disabled');
+          showError('correct');
+        // } else {
+          // showError('required');
+        // }
+
       } else {
         $('.button-submit').attr('disabled', 'disabled');
-        $('.c_register-form__validation-box__alert').removeClass('correct');
-        $('.c_register-form__validation-box__alert').addClass('error');
+        showError('error');
       }
     });
+
+
+    $('.js-register-submit').on('click', function() {
+      var form = $(this).closest('form');
+      var formValid = form[0].checkValidity();
+
+      if (formValid) {
+        form.submit();
+      } else {
+        showError('required');
+      }
+    });
+
   });
 
 }());
