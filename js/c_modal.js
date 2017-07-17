8e1073c71f2f3b125a4d0d47c694cfeb59c63902
Eduardo Sada <eduardosada@gmail.com>
2017-07-17 07:25:06 +0200

    feat(modal): agrega la x al modal de subir CV

--- js/c_modal.js
+++ js/c_modal.js
@@ -1,9 +1,23 @@
-$('.js-open-modal').on('click', function(e) {
-  var modal = $('#' + $(e.currentTarget).data('target-modal'));
+(function() {
+  window.MAIN_APP = window.MAIN_APP || [];
 
-  modal.addClass('opened');
+  window.MAIN_APP.push(function initModal() {
 
-  modal.on('click', function() {
-    modal.removeClass('opened');
-  })
-})
+    $('.js-open-modal').on('click', function(e) {
+      var modal = $('#' + $(e.currentTarget).data('target-modal'));
+
+      modal.addClass('opened');
+
+      modal.on('click', function(e) {
+        var $target = $(e.target);
+
+        if ($target.hasClass('js-modal-dismiss') || e.target === modal[0]) {
+          modal.removeClass('opened');
+          modal.off();
+        }
+      });
+    });
+
+  });
+
+}());
