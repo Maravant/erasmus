420b999128eabd9eba53e1c6ec30832d85767030
Ruth <ruthabmu@gmail.com>
2017-09-06 19:31:20 +0200

    (js/input-file):mensajes individuales adjuntar clase al input-file y anadir el icono del archivo a la nueva clase

--- js/input-file.js
+++ js/input-file.js
@@ -4,15 +4,18 @@
   window.MAIN_APP.push(function initInputFile() {
 
     var inputFileHandler = function() {
-      var span = $(this).closest('.input-file').find('span');
+      var $cmp = $(this).closest('.input-file');
+      var span = $cmp.find('span');
 
       if (this.value) {
         var path = this.value.split('\\');
         path = path[path.length - 1];
 
         span.text(path);
+        $cmp.addClass('has-value');
       } else {
         span.text(this.placeholder);
+        $cmp.removeClass('has-value');
       }
     };
 
